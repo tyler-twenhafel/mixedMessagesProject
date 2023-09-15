fa9ba8f48429770b599a134f5f489918c41a36fa
[tyler twenhafel] <tylernt42@gmail.com>
2023-09-15 18:02:12 -0400

    last commit added script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let message = '';
+const messageParts = {
+    band: ['Bleed From Within ', 'Hatebreed ', 'Crown The Empire ', 'The Amity Affliction ', 'Knocked Loose ', 'Beartooth ', 'Currents ', 'Slipknot ', 'August Burns Red '],
+    preference: ['wants to ', 'loves to ', 'hates to '],
+    actions: ['rock out ', 'release a new song ', 'start a mosh pit ', 'scream ', 'do a guitar solo ', 'rage out ', 'go skating ', 'conjure spells '],
+    places: ['on stage', 'in the pit', 'at a festival', 'on the roof', 'at a concert', 'on a bridge', 'on the tour bus'],
+}
 
+//Choose random element from each array
+const randomMessage = (array) => 
+  array[Math.floor(Math.random() * array.length)];
 
+//Assemble the message pieces into the final message
+const assembleMessage = () => {
+    let message = "";
 
-console.log(message);
+    for (let word in messageParts) {
+        message += randomMessage(messageParts[word]);
+    }
+    console.log("The band " + message + ".");
+}
+
+assembleMessage()
